9a612f453dd2d3054ff4e61e7c5df3471e0e751a
rinat salimyanov <rinatsin@gmail.com>
2020-08-25 09:01:24 +0500

    Create stretch line

--- Assets/Scripts/Dimensions.cs
+++ Assets/Scripts/Dimensions.cs
@@ -9,6 +9,7 @@ public class Dimensions : MonoBehaviour
     float distance = 0; // Расстояние от начальной до конечной точки
 
     ArrayList _dimensions = new ArrayList(); //список всех полученных измерений
+    ArrayList _dimension = new ArrayList();//Список одного измерения[distance, point1, point2]
 
     Ray ray;
     Vector3 cursorPos;
@@ -16,7 +17,7 @@ public class Dimensions : MonoBehaviour
     LineRenderer lineRenderer;
     GameObject line;
 
-
+    
     void Update()
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -30,49 +31,62 @@ public class Dimensions : MonoBehaviour
             } else if (point1 != Vector3.zero && point2 == Vector3.zero)
             {
                 point2 = hit.point;
-                OnLineRender(point1, point2);
+                StartCoroutine("updateLine");
                 saveDimensions();
                 point1 = Vector3.zero;
                 point2 = Vector3.zero;
             }
         }
-        //if (point1 != Vector3.zero && point2 == Vector3.zero && Physics.Raycast(ray, out hit))
-        //{
-        //    OnLineRender(point1, hit.point);
-        //}
+        if (point1 != Vector3.zero && point2 == Vector3.zero && Physics.Raycast(ray, out hit))
+        {
+            StartCoroutine("updateLine");
+
+        }
     }
 
     /*
      * Функция рисования линии по двум точкам
      */
-    void OnLineRender(Vector3 point1, Vector3 point2)
-    {
-        line = new GameObject();
-        lineRenderer = line.AddComponent<LineRenderer>();
-        lineRenderer.widthMultiplier = 0.1f;
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.startColor = Color.black;
-        lineRenderer.endColor = Color.black;
-        Vector3[] points = new Vector3[2] { point1, point2 };
-        lineRenderer.SetPositions(points);
-        lineRenderer.useWorldSpace = true;
-        //if (point2 == Vector3.zero)
-        //{
-        //    Destroy(lineRenderer);
-        //}
-    }
+    //void OnLineRender(Vector3 point1, Vector3 point2)
+    //{
+    //    line = new GameObject("Line");
+    //    lineRenderer = line.AddComponent<LineRenderer>();
+    //    lineRenderer.widthMultiplier = 0.1f;
+    //    lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+    //    lineRenderer.startColor = Color.black;
+    //    lineRenderer.endColor = Color.black;
+    //    Vector3[] points = new Vector3[2] { point1, point2 };
+    //    lineRenderer.SetPositions(points);
+    //    lineRenderer.useWorldSpace = true;
+    //}
 
     /*
      * Функция сохраняет полученные измерения
      */
     void saveDimensions()
     {
-        ArrayList list = new ArrayList();//Список одного измерения [distance, point1, point2]
         distance = Vector3.Distance(point1, point2);
-        list.Add(distance);
-        list.Add(point1);
-        list.Add(point2);
-        _dimensions.Add(list);
-        list.Clear();
+        _dimension.Add(distance);
+        _dimension.Add(point1);
+        _dimension.Add(point2);
+        _dimensions.Add(_dimension);
+        _dimension.Clear();
+    }
+
+    IEnumerator updateLine()
+    {
+        line = new GameObject("Line ");
+        lineRenderer = line.AddComponent<LineRenderer>();
+        lineRenderer.widthMultiplier = 0.1f;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = Color.black;
+        lineRenderer.endColor = Color.green;
+        Vector3[] points = new Vector3[2] { point1, hit.point };
+        lineRenderer.SetPositions(points);
+        if (point2 == Vector3.zero)
+        {
+            Destroy(line, Time.deltaTime);
+        }
+        yield return new WaitForSeconds(.1f);
     }
 }
