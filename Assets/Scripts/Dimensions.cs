1b1b361fd05cfd013e81a11cf4f17f2a38527896
rinat salimyanov <rinatsin@gmail.com>
2020-09-01 04:31:44 +0500

    Line stand alone

--- Assets/Scripts/Dimensions.cs
+++ Assets/Scripts/Dimensions.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Dimensions : MonoBehaviour
 {
+    public float lineWidth = .1f; //ширина линии
+    int n = 1;// номер линии
+
     Vector3 point1 = Vector3.zero; //Начальная точка измерения
     Vector3 point2 = Vector3.zero; //Конечная точка измерения
     float distance = 0; // Расстояние от начальной до конечной точки
@@ -12,10 +15,12 @@ public class Dimensions : MonoBehaviour
     ArrayList _dimension = new ArrayList();//Список одного измерения[distance, point1, point2]
 
     Ray ray;
-    Vector3 cursorPos;
     RaycastHit hit;
     LineRenderer lineRenderer;
     GameObject line;
+    BoxCollider lineCollider;
+    Ray raySelect;
+    RaycastHit hitSelect;
 
 
     void Update()
@@ -33,6 +38,7 @@ public class Dimensions : MonoBehaviour
             {
                 point2 = hit.point;
                 StartCoroutine(updateLine(point1, point2));
+                addCollider(lineRenderer, point1, point2);
                 saveDimensions();
                 point1 = Vector3.zero;
                 point2 = Vector3.zero;
@@ -42,6 +48,15 @@ public class Dimensions : MonoBehaviour
         {
             StartCoroutine(updateLine(point1, hit.point));
         }
+        if (Input.GetMouseButtonDown(1))
+        {
+            raySelect = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(raySelect, out hitSelect) && hitSelect.collider.tag == "Line")
+            {
+                lineRenderer.startColor = Color.blue;
+                lineRenderer.endColor = Color.blue;
+            }
+        }
     }
 
     /*
@@ -50,6 +65,7 @@ public class Dimensions : MonoBehaviour
     void saveDimensions()
     {
         distance = Vector3.Distance(point1, point2);
+        print(distance);
         _dimension.Add(distance);
         _dimension.Add(point1);
         _dimension.Add(point2);
@@ -59,14 +75,17 @@ public class Dimensions : MonoBehaviour
 
     IEnumerator createLine(Vector3 p1, Vector3 p2)
     {
-        line = new GameObject("Line ");
+        line = new GameObject("Line " + n);
+        line.tag = "Line";
         lineRenderer = line.AddComponent<LineRenderer>();
-        lineRenderer.widthMultiplier = 0.1f;
+        lineRenderer.widthMultiplier = lineWidth;
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.startColor = Color.black;
         lineRenderer.endColor = Color.green;
         Vector3[] points = new Vector3[2] { p1, p2 };
         lineRenderer.SetPositions(points);
+        line.transform.position = (p1 + p2) / 2;
+        n++;
         yield return null;
     }
 
@@ -76,5 +95,31 @@ public class Dimensions : MonoBehaviour
         lineRenderer.SetPositions(points);
         yield return null;
 
+    }
+
+    void addCollider(LineRenderer lineRend, Vector3 p1, Vector3 p2)
+    {
+        if (line != null)
+        {
+            lineCollider = line.AddComponent<BoxCollider>();
+            lineCollider.transform.parent = lineRend.transform;
+            float lineWidth = lineRend.endWidth / 2;
+            float lineLength = Vector3.Distance(p1, p2);
+            lineCollider.size = new Vector3(lineLength, lineWidth, 1f);
+            float angle = Mathf.Atan2((p2.z - p1.z), (p2.x - p1.x));
+            angle *= Mathf.Rad2Deg;
+            angle *= -1;
+            lineCollider.transform.Rotate(0, angle, 0);
+        }
+    }
+
+    //Параметрическое уравнение прямой для нахождения любой точки лежащей на этой прямой
+    Vector3 findLinePoint(Vector3 p1, Vector3 p2, float t)
+    {
+        Vector3 vector = new Vector3((p2.x - p1.x), (p2.y - p1.y), (p2.z - p1.z));
+        float x = vector.x * t + p1.x;
+        float y = vector.y * t + p1.y;
+        float z = vector.z * t + p1.z;
+        return new Vector3(x, y, z);
     }
 }
